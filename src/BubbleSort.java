8084251487d9f447472d54b87345a00c89b81ed6
Panneerselvam77 <panneerdeveloper77@gmail.com>
2025-02-26 07:53:19 +0530

    MergeSort & SelectionSort added

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -1,28 +1,60 @@
-public class BubbleSort {
-    public static void main(String[] arg){
-        int nums[] = {6,3,7,2,15, 9};
-        int size = nums.length;
-        int temp = 0;
-        System.out.println("Before sorting");
-        for(int num : nums){
-            System.out.print(num + " ");
-        }
+import java.util.Arrays;
 
-        for (int i = 0; i < size; i++){
-            for(int j = 0; j < size - i - 1; j++){
-              if(nums[j] > nums[j + 1]){
-                  temp = nums[j];
-                  nums[j] = nums[j +1];
-                  nums[j + 1] = temp;
-              }
+//public class BubbleSort {
+//    public static void main(String[] arg){
+//        int nums[] = {6,3,7,2,15, 9};
+//        int size = nums.length;
+//        int temp = 0;
+//        System.out.println("Before sorting");
+//        for(int num : nums){
+//            System.out.print(num + " ");
+//        }
+//
+//        for (int i = 0; i < size; i++){
+//            for(int j = 0; j < size - i - 1; j++){
+//              if(nums[j] > nums[j + 1]){
+//                  temp = nums[j];
+//                  nums[j] = nums[j +1];
+//                  nums[j + 1] = temp;
+//              }
+//            }
+//        }
+//        System.out.println();
+//        System.out.println("After sorting");
+//        for (int num :nums){
+//            System.out.print(num + " ");
+//        }
+//// Need to check Time complexity
+//
+//    }
+//}
+
+public class BubbleSort {
+    private static void bubbleSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            boolean swap = false;
+            for (int j = 0; j < arr.length - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    swap = true;
+                    // Swapping values
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+            // If no swap happens, array is already sorted
+            if (!swap) {
+                break;
             }
         }
-        System.out.println();
-        System.out.println("After sorting");
-        for (int num :nums){
-            System.out.print(num + " ");
-        }
-// Need to check Time complexity
+    }
 
+    public static void main(String[] arg) {
+        int[] arr = {17, 7, 97, 53, 77, 49, 3};
+        System.out.println("Before sorting: " + Arrays.toString(arr));
+
+        bubbleSort(arr);
+
+        System.out.println("After sorting: " + Arrays.toString(arr));
     }
 }
--- src/SelectionSort.java
+++ src/SelectionSort.java
@@ -1,40 +1,72 @@
-public class SelectionSort {
-    // Need to check Time complexity
-    public static void main(String[] arg){
+//public class SelectionSort {
+//    // Need to check Time complexity
+//    public static void main(String[] arg) {
+//
+//        int[] nums = {6,3,7,2,15, 9};
+//        int size = nums.length;
+//        int temp = 0, minIndex = -1;
+//
+//        System.out.println("Before sorting");
+//        for(int num : nums) {
+//            System.out.print(num + " ");
+//        }
+//
+//        for(int i = 0; i < size-1 ; i++) {
+//            minIndex = i;
+//            for(int j = i + 1; j < size; j++) {
+//                if(nums[minIndex] > nums[j]) {
+//                    minIndex = j;
+//                }
+//            }
+//            temp = nums[minIndex];
+//            nums[minIndex] =  nums[i];
+//            nums[i] = temp;
+//            System.out.println();
+//            for (int num :nums) {
+//                System.out.print(num + " ");
+//            }
+//        }
+//
+//
+//        System.out.println();
+//        System.out.println("After sorting");
+//        for (int num :nums) {
+//            System.out.print(num + " ");
+//        }
+//
+//
+//    }
+//}
 
-        int[] nums = {6,3,7,2,15, 9};
-        int size = nums.length;
-        int temp = 0;
+
+public class SelectionSort {
+    public static void main(String[] arg){
+        int[] arr = { 64, 25, 12, 22, 11 };
+        int size = arr.length;
         int minIndex = -1;
 
         System.out.println("Before sorting");
-        for(int num : nums){
+        for(int num : arr) {
             System.out.print(num + " ");
         }
 
-for(int i = 0; i < size-1 ; i++){
+
+for(int i = 0; i < size -1; i++){
     minIndex = i;
     for(int j = i + 1; j < size; j++){
-if(nums[minIndex] > nums[j]){
-    minIndex = j;
-}
+        if(arr[minIndex] > arr[j]){
+            minIndex = j;
+        }
     }
-    temp = nums[minIndex];
-    nums[minIndex] =  nums[i];
-    nums[i] = temp;
-    System.out.println();
-    for (int num :nums){
-        System.out.print(num + " ");
-    }
+    int temp = arr[minIndex];
+    arr[minIndex] = arr[i];
+    arr[i] = temp;
 }
 
-
-        System.out.println();
-        System.out.println("After sorting");
-        for (int num :nums){
+        System.out.println("\nAfter sorting");
+        for (int num :arr) {
             System.out.print(num + " ");
         }
-
 
     }
 }
